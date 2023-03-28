4dcdb7be7b0744c80e2730da6a71c81504d83b43
obfemiojo <obafemiojo97@gmail.com>
2023-03-28 13:53:06 +0100

    new model

--- weather.py
+++ weather.py
@@ -19,8 +19,13 @@ Search_image=PhotoImage(file="search.png")
 myimage=Label(image=Search_image)
 myimage.place(x=20,y=20)
 
+textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
 
-
+Search_icon=PhotoImage(file="search_icon.png")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040")
+myimage_icon.place(x=400,y=34)
 
 
 
